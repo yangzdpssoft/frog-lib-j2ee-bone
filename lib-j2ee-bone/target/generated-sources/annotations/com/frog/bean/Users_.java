60eb5a3198639cd113bd3aaa59327dc940e79b46
yangzdpssoft <yangzdpssoft@163.com>
2014-07-24 10:25:53 +0800

    init

---
+++ lib-j2ee-bone/target/generated-sources/annotations/com/frog/bean/Users_.java
@@ -0,0 +1,15 @@
+package com.frog.bean;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Users.class)
+public abstract class Users_ {
+
+	public static volatile SingularAttribute<Users, Integer> id;
+	public static volatile SingularAttribute<Users, String> name;
+
+}
+

6082bef12448c138af17eebf749582b4ce9392af
yangzdpssoft <yangzdpssoft@163.com>
2014-08-21 10:46:22 +0800

    init

--- lib-j2ee-rails-mongodb/src/main/webapp/static/admin/service.js
+++ lib-j2ee-rails-mongodb/src/main/webapp/static/admin/service.js
@@ -1,7 +1,14 @@
 app.service('_ajax', function($http){
-    this.postCall = function(url){
-        $http({method : 'post', url : url}).success(function(data, status){
+    this.postCall = function(url, data){
+        var result = null;
+        $http({method : 'post', data : data, url : url}).success(function(response, status){
+            if(isSuccess(response)){
+                result = response.data;
+            }else if(isError(response) || isFailure(response)){
 
+            }else if(isLogin(response)){
+
+            }
         }).error(function(data, status) {
 
         });

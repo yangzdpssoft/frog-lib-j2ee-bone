340459b1fb124fc289c90c60104210cbba1704b7
yangzdpssoft <yangzdpssoft@163.com>
2014-09-05 15:36:07 +0800

    init

--- lib-j2ee-rails-mongodb/src/main/webapp/template/admin/define/tree.ftl
+++ lib-j2ee-rails-mongodb/src/main/webapp/template/admin/define/tree.ftl
@@ -9,24 +9,30 @@
 <body>
 <div id="layout" class="easyui-layout" fit="true">
     <div id="west" data-options="region:'west',split:true" style="width:20%;">
-        <input type="text" placeholder="快速检索" style="margin: 0px;border-radius: 0px;min-height: 0px;"/>
-        <ul edittree class="easyui-tree" data-options="url:'${ctx}/template/admin/define/tree_data1.json'"></ul>
-        <div id="westMenu" class="easyui-menu" style="width: 150px;">
-            <div id="tree_refresh" iconCls="icon-reload">
+        <input treeSearch="tree" type="text" placeholder="快速检索" style="margin: 0px;border-radius: 0px;min-height: 0px;"/>
+        <ul id="tree" edittree class="easyui-tree" data-options="url:'${ctx}/template/admin/define/tree_data1.json'"></ul>
+        <div treeContentMenu="west_tree" class="easyui-menu" style="width: 150px;">
+            <div id="tree_reload" treeContextMenuButton iconCls="icon-reload">
                 刷新
             </div>
-            <div id="tree_refresh" iconCls="icon-save">
+            <div id="tree_drag" treeContextMenuButton iconCls="icon icon-drag">
+                拖动模式
+            </div>
+            <div id="tree_save" treeContextMenuButton iconCls="icon-save">
                 保存
             </div>
             <div class="menu-sep"></div>
-            <div id="tree_add" iconCls="icon-add">
-                添加
+            <div id="tree_rename" treeContextMenuButton iconCls="icon-edit">
+                重命名
             </div>
-            <div id="tree_edit" iconCls="icon-edit">
-                编辑
+            <div id="tree_add" treeContextMenuButton iconCls="icon-add">
+                添加节点
             </div>
-            <div id="tree_del" iconCls="icon-cancel">
-                删除
+            <div id="tree_subadd" treeContextMenuButton iconCls="icon-add">
+                添加子节点
+            </div>
+            <div id="tree_del" treeContextMenuButton iconCls="icon-cancel">
+                删除节点
             </div>
         </div>
     </div>
--- lib-j2ee-rails-mongodb/src/main/webapp/template/admin/define/tree.js
+++ lib-j2ee-rails-mongodb/src/main/webapp/template/admin/define/tree.js
@@ -7,22 +7,123 @@
                 fit : true
             });
         });
-        $("#west").bind('contextmenu',function(e){
-            $('#westMenu').menu('show', {
-                left: e.pageX,
-                top: e.pageY
+        $("[treeSearch]").each(function(){
+            var tree = $('#' + $(this).attr('treeSearch'));
+            var text = $(this);
+            text.keydown(function(){
+                var url = tree.tree('options').url;
+                var i = url.indexOf('?');
+                if(i != -1){
+                    var u = url.substr(0, i);
+                    tree.tree('options').url = u + '?keyword=' + text.val();
+                    tree.tree('reload');
+                }else{
+                    tree.tree('options').url = url + '?keyword=' + text.val();
+                    tree.tree('reload');
+                }
             });
-            return false;
         });
-        $("[edittree]").bind('contextmenu',function(e){
-            $('#westMenu').menu('show', {
-                left: e.pageX,
-                top: e.pageY
+        $("[treeContentMenu]").each(function(){
+            var menu = $(this);
+            var treeContentMenu = $(this).attr('treeContentMenu');
+            var d = treeContentMenu.split('_');
+            menu.bind('contextmenu', function(e){
+                return false;
             });
-            return false;
+            $.each(d, function(){
+                $('#' + this).bind('contextmenu',function(e){
+                    menu.menu('show', {
+                        left: e.pageX,
+                        top: e.pageY
+                    });
+                    return false;
+                });
+            });
         });
-        $("#westMenu").bind('contextmenu',function(e){
-            return false;
+        $("[treeContextMenuButton]").click(function(){
+            var contextButton = $(this).attr('id');
+            var d = contextButton.split('_');
+            var tree = $('#' + d[0]);
+            var act = d[1];
+            if(act === 'add'){
+                var node = tree.tree('getSelected');
+                var n = {
+                    id: -1,
+                    text: ''
+                };
+                if (node){
+                    tree.tree('insert', {
+                        after: node.target,
+                        data: n
+                    });
+                    var newNode =  tree.tree('find', -1);
+                    tree.tree('update', {target: newNode.target, id : ''});
+                    tree.tree('beginEdit', newNode.target);
+                }else{
+                    $.messager.show({title: '警告', msg:  '请选中一个节点！'});
+                }
+            }else if(act === 'subadd'){
+                var node = tree.tree('getSelected');
+                var n = {
+                    id: -1,
+                    text: ''
+                };
+                if (node){
+                    tree.tree('append', {
+                        parent: node.target,
+                        data:n
+                    });
+                    var newNode =  tree.tree('find', -1);
+                    tree.tree('update', {target: newNode.target, id : ''});
+                    tree.tree('beginEdit', newNode.target);
+                }else{
+                    $.messager.show({title: '警告', msg:  '请选中一个节点！'});
+                }
+            }else if(act === 'rename'){
+                var node = tree.tree('getSelected');
+                if (node){
+                    tree.tree('beginEdit', node.target);
+                }else{
+                    $.messager.show({title: '警告', msg:  '请选中一个节点！'});
+                }
+            }else if(act === 'del'){
+                var node = tree.tree('getSelected');
+                if (node){
+                    tree.tree('remove', node.target);
+                }else{
+                    $.messager.show({title: '警告', msg:  '请选中一个节点！'});
+                }
+            }else if(act === 'drag'){
+                var drag = $(this).text().trim();
+                var itemEl = $('#' + contextButton)[0];
+                var item = $(this).parent().menu('getItem', itemEl);
+                if(drag === '拖动模式'){
+                    tree.tree('enableDnd');
+                    $(this).parent().menu('setText', {
+                        target: item.target,
+                        text: '取消拖动'
+                    });
+                    $(this).parent().menu('setIcon', {
+                        target: itemEl,
+                        iconCls: 'icon icon-undrag'
+                    });
+                }else if(drag === '取消拖动'){
+                    tree.tree('disableDnd');
+                    $(this).parent().menu('setText', {
+                        target: item.target,
+                        text: '拖动模式'
+                    });
+                    $(this).parent().menu('setIcon', {
+                        target: itemEl,
+                        iconCls: 'icon icon-drag'
+                    });
+                }
+            }else if(act === 'reload'){
+                tree.tree('reload');
+            }
+            else if(act === 'save'){
+                tree.tree('reload');
+            }
         });
     });
 })(jQuery);
